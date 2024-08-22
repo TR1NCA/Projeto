7ad76eca582ddd429992e23fd90452f7ce00ab2f
TR1NCA <fernando3f.2005@gmail.com>
2024-08-22 14:02:31 -0300

    Add files via upload

---
+++ projeto1.py
@@ -0,0 +1,122 @@
+textoc = "NOSSO POVO.1 2Aqui começa a história do tempo dos antigos.3O início da história.4Antes do mundo existir.5Ngutapa já existia.6Ele não teve pai nem mãe.7Mapana.8a mulher de Ngutapa.9se criou junto com ele.10No mesmo lugar viviam também Baia e sua mulher.11Baia era parente de Ngutapa.12No lugar onde esses quatro se criaram é onde ficava a montanha Taiwegüne.13E no igarapé Tonetü.14Naquele tempo.15a terra ainda estava se formando.16O mato era baixinho e o rio ainda tinha pouca água17lá eles viviam. 18Passaram-se muitos anos. 19gutapa e Mapana nunca habitaram juntos.20nca tiveram filhos.21Um dia.22quando o mato já estava crescido.23Ngutapa foi caçar com Mapana.24No caminho eles começaram a brigar. 25Ngutapa agarrou sua mulher e lhe deu uma surra.26Depois disso.27amarrou Mapana num pau.28de braços e pernas abertos. 29Deixou ela ali e seguiu para caçar no mato.30Vieram as cabas e as formigas e morderam a sua periquita.31Ela sofreu muito.32Então apareceu o pássaro cancã e se sentou no alto do pau onde ela estava amarrada.33Mapana disse para o cancã:.34- Vovó.35pode me desamarrar?.36- O pássaro gritou:.37- Co, co, co, cou!.38- Vovó.39venha me desamarrar.40Aquele desgraçado me prendeu aqui para me matar.41- ela falou de novo.42O cancã se transformou em gente e.43chegando mais perto.44perguntou:.45- O que lhe aconteceu.46minha neta?.47Se você quiser se vingar de Ngutapa.48está aqui a caba.49Ela pegou a caba e guardou.50A casa de caba era muito grande.51mas parecia pequena.52O Cancã falou ainda:.53- Você não pode ficar aí.54Tem que esperar o seu marido num lugar onde ele não possa ver.55Depois disso.56o cancã se transformou em pássaro e foi embora.57Demorou um pouco e Ngutapa voltou da caçada.58Vinha tocando flauta e pulava numa perna e noutra.59cantando:.60- Por onde anda Mapana?!.61As cabas e as formigas morderam a periquita dela!.62Tcheruru tcherμru-u-u-u ... Tcheruru tcheruru-u-u-u ... - assim dizia.63Mapana estava escondida num tronco de árvore.64esperando Ngutapa passar.65Escutou esse canto e se preparou.66Quando ele chegou.67ela jogou a casa de caba em cima dele e acertou-lhe os dois joelhos.68Ngutapa caiu e não se levantou mais.69Mapana deixou ele ali mesmo e foi embora.70Ngutapa foi se arrastando até em casa.71Desde que as cabas ferraram.72seus joelhos começaram a inchar.72"
+
+textotc= "TORÜ DUǕ'ǕGÜ.1 2Nori arü ügü ga ore ga nucüma'ǖ ga torü ga na nhunhaacü yi'i'ǖ.3Norü ügü tchiga.4Nüma ga Ngutapa ga naãne nama'ã ya ĩĩtchicü.5rü nge'eǖ rü na tauma i ta'acü i nhũma na yema i na i aẽǖrü'ǖ.6Rü yiĩ'ǖ ngema na yamareǖ ga Ngutapa.7I nori'ǖtchicü.8ingeguma.9rü norü tchiura ta ngemana yaǖ.10rü ya Baia rü ta cünanangemaǖ rü norü tchiura rü ta nangemaǖ.11Erü nüma ga Baia rü Ngutapa tanü'ǖ ni'ĩ rü wü'iva na yae.12Nucüma ngeguma rü numa ga Ngutapa rü na tauǖma ga norü bue nu'ǖ tayema yerü tauguma nügüga nidau.13ga namamaã rü na yaemare.14Ngeguma yema namamare i ya'ĩ.15rü tauguma i buǖã.16Ga ngĩma ga Mapana.17rü yemaca ngitchi naai ga nüma ga Ngutapa.18Inacü i ngeguma i ngema.19I Taiwegüne i nori naãene i üǖwacü rü ngema ni'ĩ i nangemagüǖ erü ngema i nori naü'ǖ i naãene nua natü i Tonetü.20Erü yeguma ãrü naãenecü rü wü'i i ngewaca i ya eaneǖrǖ'ǖ ni'ĩ yerü natauma ga nhãtü rü yema nawa nayemagüǖ ricatama ni'ĩ.21Rü yeguma ngemamã ni'ĩ i nangemagü'ǖ.22rü marü nhurema ya taunecǖ na yemagü rü yegumama ni'ĩ ga tüǖ nacuaiǖ ga namã ga Ngutapa rü yeguma rü na tauma ga to.23ga duǖ'ǖ nüĩcagümare ni'ĩ ga nayemagü'ǖ.24Natürü ga guma Baia rü nü'ǖ tayema ga bue rü naaãcü nǖetcha nüma ga guma.25Ngutapa rü tauguma naãcü rü mu'utchicüma ya tauenecü na ngeacü.26Rü yeguma fenewa tümamãã na u'ǖ ga namá ga Ngutapa.27yerü yeguma rü marü nayaanegü ga naĩnecü rü yema i na feneeügu ni'ĩ ga tü'ǖ na cuai'ǖ ga nama.28rü yeguma tü'ǖ ya naĩ'ü rü nainewa.29tü'ǖ ni'ĩ nai rü ngiparawa rüta tü'ǖ ni'ĩ naĩgü rü ngĩtchacü'ǖwa rüta.30Rü yemawena rü nüma ga.31Ngutapa rü na üpetü ga na ya feneǖ.32Yerü yegumarü marü auriǖtchima nguǖ tinge rü maẽ rü tü'ǖ nawi rü ẽne rü tü'ü na ngo ga tümaãrü ngaǖwa.33Rü Yeguma tümaetügu i na rü wagügü i coou rü ngeguma ngina tacagü rü nhãtagürü:.34- Pa noẽ.35pa coou tautchiname'ǖ ega tchoǖ icuwẽgǖgu nhãtagürü co-co-co-cou nhatagüǖ?.36- tauchina meǖ ega tchoǖ icuiwẽgügu rü tchoǖ ni'ĩ ma i ucaǖǖtchi i Ngutapa nhãtagürü:.37- Rü yeguma ga noẽ ga Co, co, co, cou rü ĩrü duǖ!.38- pa noẽ.39Rü yeguma rü tü'ǖ ĩ ya wẽgü.40Rü ngeguma cuütanütchaǖgu rü daani'ĩ ya maẽ nhanagürü.41- rü yeguma rü inayau ga maẽ ga Mapana.42- Rü yeguma ga noẽ i coou ta ngica itarü ĩ.43rü ngiǖ ta ngaǖ rü cu tücü.44pa tchauta'ã nhãtagürü:.45- Natürü ga guma maẽ rü naega.46rü ngerata ni'ĩ ga naẽga.47natütü guma maẽ.48rü na taütchirema.49Rü natürü yiema i daugu rü na i'ra.50Rü yeguma i nayau ga maẽ.51rü ngigürügü taũǖ tama ni'ĩ nua.52cuǖna ngẽẽǖ ngigürügü:.53Rü marü yemawena ga coou rü tüna i ĩgü rü weriǖ ti'igüãrü.54Rü yeguma ga nüma ga Ngutapa rü marü na taegutchaǖ.55Rü yeguma marü nataegugu rü wowerugu ni'ĩ fetchigü.56rü yeguma ga ngima ga Mapana rü marü iya nguẽẽ ga namacüwawa ga naĩpünewa rü nüma ga Ngutapa rü ni'ĩ woweruetchigü.57Rü ni'ĩ wü'i paratchigü.58rü ni'ĩ fenagütchigü.59rü nhãnagürü:.60- nhũmarütchi nhũnhã i Mapana?!.61i maẽ ngi'ǖ wiarü ngaǖãcü rü ẽne ngi'ǖ ngo arü nga'uãcü nhãnagürü!.62Tcherutcheru-u-u-u ... tcherurutcheruru-u-u-u ... - nhanagürü.63Rü yeguma ga tüma ga Mapana rü marü nüǖ ta inü'ǖ.64Rü wüiparagu ni'ĩ ga nüma rü yeguma marü na ngupetüatchigu.65rü nügü i'ya caiãtchigu rü yeguma naweama nenanhãga guma maẽ.66Rü pe ga naã pü'ügu rü norü naiãpüüguta.67Rü ngǖ ga ini'ĩ ni'ĩngu.68rü marü tama yema i natchi rü tüma ga norü na'ma ga Mapana.69rü tüma i ti'u rü yema yirüma ni'ĩ rü nüna iti'ãma ga ĩpatawa.70Rü yeguma ga nüma rü marü taguma i natchi rü nüma ga naãpüǖ rü marü manarütchaapüǖ.71rü yemaãcü tama ini'ĩu rü ini waatchigü.72Rü nhumata inangu ga ĩpatawa ga nayaanegü.72"
+
+frase = 1
+frase2 = 1
+
+frase_atual = f""
+frase_atual2 = f""
+
+frase_atua = f"{frase}"
+frase_atua2 = f"{frase2}"
+
+ptbr = ""
+ticuna = ""
+
+textotc_original = ""
+
+textoc_original = ""
+
+
+def ler_texto1():
+    print("Digite o texto em ptbr(pressione Enter em uma linha vazia para finalizar):")
+    texto = ""
+    linha = "a"
+    while linha != "":
+        linha = input()
+        texto += linha + "\n"
+
+ler_texto1()
+
+
+def ler_texto2():
+    print("Digite o texto em ticuna (pressione Enter em uma linha vazia para finalizar):")
+    texto = ""
+    linha = "a"
+    while linha != "":
+        linha = input()
+        texto += linha + "\n"
+
+
+ler_texto2()
+
+
+if ler_texto1 != textoc:
+    print("texto em pt está errado corrigindo...")
+    textoc_original = textoc
+if ler_texto2 != textotc:
+    print("texto em ticuna está errado corrigindo...")
+    textotc_original = textotc
+
+print("--------------------------------------------------------------Você quer uma frase específica?--------------------------------------------------------------")
+print("                                                                          Y/N                                                                              ")
+frasetxt = input().upper()
+
+if frasetxt == "N":
+    print( "------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+    while frase2 != 73:
+        for char2 in textotc_original:
+            if f"{frase2}" in frase_atua2:
+                if char2 in f".":
+                    frase2 += 1
+                    ticuna = frase_atual2
+                    frase_atual2 = ""
+                    break
+                else:
+                    frase_atual2 += char2
+            else:
+                frase_atua2 += char2
+
+        for char in textoc_original:
+            if f"{frase}" in frase_atua:
+                if char == f".":
+                    frase += 1
+                    ptbr = frase_atual
+                    frase_atual = ""
+                    break
+                else:
+                    frase_atual += char
+            else:
+                frase_atua += char
+        print(ptbr)
+        print(ticuna)
+        print("------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+
+
+else:
+    print("--------------------------------------------------------------coloque a frase escoolhida--------------------------------------------------------------")
+    frasee = input()
+    while frase2 != 74:
+        if frasee == ptbr or frasee == ticuna:
+            print(ptbr)
+            print(ticuna)
+            frase2 = 74
+            print("------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+        else:
+            for char2 in textotc_original:
+                if f"{frase2}" in frase_atua2:
+                    if char2 in f".":
+                        frase2 += 1
+                        ticuna = frase_atual2
+                        frase_atual2 = ""
+                        break
+                    else:
+                        frase_atual2 += char2
+                else:
+                    frase_atua2 += char2
+
+            for char in textoc_original:
+                if f"{frase}" in frase_atua:
+                    if char == f".":
+                        frase += 1
+                        ptbr = frase_atual
+                        frase_atual = ""
+                        break
+                    else:
+                        frase_atual += char
+                else:
+                    frase_atua += char
